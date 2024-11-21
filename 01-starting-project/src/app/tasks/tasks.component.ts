ea1a71544872c4c8e4cb6a144d2270ef369b1ebf
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-21 10:11:00 -0600

    Output user specific tasks

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,14 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-@Input() name?:string; // it might not be initiallized yet
+@Input({required: true}) userId!:string;// we sue required true because we KNOW we will get values for these properties
+@Input({required: true}) name!:string; // it might not be initiallized yet
 //@Input() name:string | undefined; //can also use a union as weel
+tasks = [
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+]
 
+get selectedUserTasks() {
+  return this.tasks.filter((task) => task.userId === this.userId);
+}
 }
