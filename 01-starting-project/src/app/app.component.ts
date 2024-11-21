4094584b89526ea270ad1bbb314dc8610513105f
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-21 08:31:25 -0600

    Add tasks to the application

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from '../../src/app/dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find( x => x.id === this.selectedUserId)!;// the bang is added because typescript doesnt know if it was for sure find a user  in the find
+  }
 
   onSelectUser(id: any) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
