c72c5afc41ccd2a445413c5dbf7c9b4e0db73b26
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-21 11:31:21 -0600

    Add code to delete flat tasks

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './Tasks.model';
 
 @Component({
@@ -10,5 +10,10 @@ import { Task } from './Tasks.model';
 })
 export class TaskComponent {
 @Input({required: true})task!:Task; //we will always have a task
+@Output() complete = new EventEmitter<string>();
 
+
+onCompleteTask(){
+  this.complete.emit(this.task.id);
+}
 }
